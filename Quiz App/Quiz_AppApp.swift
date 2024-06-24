a6b932fedccbf2b9b804572be20b690710e09817
Meja Santos <mejasantos23@gmail.com>
2024-06-24 14:36:25 -0400

    Initial Commit

---
+++ Quiz App/Quiz_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_AppApp.swift
+//  Quiz App
+//
+//  Created by Meja Santos on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
